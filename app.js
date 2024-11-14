dbb30fdf1770ff940d9968a0dcabbcb55bb83578
Jasmin Gubeljic <jasmin.g@tutanota.com>
2024-11-14 13:32:50 +0100

    organize routes, models and controllers

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const cors = require('cors')
+const shopRoutes = require('./routes/shop')
 
 const app = express()
+const port = process.env.PORT || 3001
+
 app.use(cors());
 app.use(express.json())
 
-const port = process.env.PORT || 3001
+app.use(shopRoutes)
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
---
+++ controllers/shop.js
@@ -0,0 +1,3 @@
+module.exports.getProducts = (req, res, next) => {
+    res.status(200).send({"some": "data"})
+}
