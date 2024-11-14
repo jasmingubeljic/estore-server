ddf3c8b20e4e283da82026b8c548d2b76cb1be14
Jasmin Gubeljic <jasmin.g@tutanota.com>
2024-11-14 09:31:08 +0100

    basic server setup

---
+++ App.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors());
+app.use(express.json())
+
+const port = process.env.PORT || 3001
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
