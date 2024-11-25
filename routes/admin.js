346adae65a378ee265685becbafad118b53a21e3
Jasmin Gubeljic <jasmin.g@tutanota.com>
2024-11-25 14:47:08 +0100

    update routes

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const adminController = require('../controllers/admin')
 const { body } = require('express-validator')
+const isAuth = require('../middleware/isAuth')
 
-const validate = [
+const validateCreateProduct = [
     body('title').trim().notEmpty().withMessage('Please enter the title of the product'),
     body('description').trim().notEmpty().withMessage('Please enter product description'),
     body('price').trim().notEmpty().withMessage('Please enter the price'),
@@ -11,8 +12,8 @@ const validate = [
     body('category').trim().notEmpty().withMessage('Plese choose category to which this product belongs')
 ]
 
-router.post('/product', validate, adminController.createProduct)
-router.put('/product/:id', adminController.updateProduct)
-router.delete('/product/:id', adminController.deleteProduct)
+router.post('/product', isAuth, validateCreateProduct, adminController.createProduct)
+router.put('/product/:id', isAuth, adminController.updateProduct)
+router.delete('/product/:id', isAuth, adminController.deleteProduct)
 
 module.exports = router
