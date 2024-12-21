6b0af24ce1953f56627853c886d99a660fa65783
Jasmin Gubeljic <jasmin.g@tutanota.com>
2024-12-21 16:49:18 +0100

    feat: add ability to offset and limit product query

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,19 +1,25 @@
-const Product = require('../models/product')
+const Product = require("../models/product");
 
 module.exports.getProducts = async (req, res, next) => {
-    const products = await Product.findAll()
-    res.status(200).json(products)
-}
+  const queryOptions = { order: [["updatedAt", "DESC"]] };
+
+  if (/^\d+$/.test(req.query.offset)) {
+    queryOptions["offset"] = req.query.offset;
+  }
+  if (/^\d+$/.test(req.query.limit)) {
+    queryOptions["limit"] = req.query.limit;
+  }
+  const products = await Product.findAll(queryOptions);
+  res.status(200).json(products);
+};
 
 module.exports.getProductById = async (req, res, next) => {
-    const product = await Product.findByPk(req.params.id)
+  const product = await Product.findByPk(req.params.id);
 
-    if (product === null) {
-        // not found
-        res.status(404).json({message: "Product not found"})
-    } else {
-        res.status(200).json(product)
-    }
-
-    
-}
+  if (product === null) {
+    // not found
+    res.status(404).json({ message: "Product not found" });
+  } else {
+    res.status(200).json(product);
+  }
+};
