3303a1f89365b2599507c23ded5bf04deaad6989
Jasmin Gubeljic <jasmin.g@tutanota.com>
2024-12-23 15:07:57 +0100

    feat: create category model

---
+++ models/category.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../util/database");
+
+const Category = sequelize.define("category", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  isHidden: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+  },
+});
+
+module.exports = Category;
