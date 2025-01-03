51a4fb04c3b140aef4eedc245fe29f9d38a94b7d
Jasmin Gubeljic <jasmin.g@tutanota.com>
2025-01-03 20:24:06 +0100

    perform: use one query for prods and count

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,9 +10,8 @@ module.exports.getProducts = async (req, res, next) => {
   if (/^\d+$/.test(req.query.limit)) {
     queryOptions["limit"] = req.query.limit;
   }
-  const products = await Product.findAll(queryOptions);
-  const totalProductCount = await Product.count();
-  res.status(200).json({ products, totalProductCount });
+  const products = await Product.findAndCountAll(queryOptions);
+  res.status(200).json({ products });
 };
 
 module.exports.getProductById = async (req, res, next) => {
@@ -23,27 +22,6 @@ module.exports.getProductById = async (req, res, next) => {
     res.status(404).json({ message: "Product not found" });
   } else {
     res.status(200).json(product);
-  }
-};
-
-module.exports.getCategories = async (req, res, next) => {
-  const categories = await Category.findAll();
-
-  if (categories) {
-    res.status(200).json(categories);
-  } else {
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-};
-
-module.exports.getCategoryById = async (req, res, next) => {
-  const category = await Category.findByPk(req.params.id);
-
-  if (category === null) {
-    // not found
-    res.status(404).json({ message: "Category not found" });
-  } else {
-    res.status(200).json(category);
   }
 };
 
@@ -71,4 +49,25 @@ module.exports.queryProducts = async (req, res, next) => {
 
   const products = await Product.findAndCountAll(queryOptions);
   res.status(200).json({ products });
+};
+
+module.exports.getCategories = async (req, res, next) => {
+  const categories = await Category.findAll();
+
+  if (categories) {
+    res.status(200).json(categories);
+  } else {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports.getCategoryById = async (req, res, next) => {
+  const category = await Category.findByPk(req.params.id);
+
+  if (category === null) {
+    // not found
+    res.status(404).json({ message: "Category not found" });
+  } else {
+    res.status(200).json(category);
+  }
 };
