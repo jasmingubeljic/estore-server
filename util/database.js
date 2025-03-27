6ace53057057537b5fc3aa7c4076acf724883adf
Jasmin Gubeljic <jasmin.g@tutanota.com>
2025-03-27 14:54:19 +0100

    build: vercel configuration for deployment

--- util/database.js
+++ util/database.js
@@ -1,10 +1,18 @@
+const pg = require("pg");
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: "postgres",
   // host: "localhost",
   host: process.env.DB_HOST,
+  dialectModule: pg,
   logging: console.log,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
