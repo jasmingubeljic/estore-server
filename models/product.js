7009855c946f53ce279e11c8a8a18512dad7bd4d
Jasmin Gubeljic <jasmin.g@tutanota.com>
2025-04-04 16:01:07 +0200

    fix: datatype for description

--- models/product.js
+++ models/product.js
@@ -24,7 +24,7 @@ const Product = sequelize.define("product", {
     allowNull: false,
   },
   description: {
-    type: Sequelize.STRING,
+    type: Sequelize.TEXT,
     allowNull: false,
   },
   isHidden: {
